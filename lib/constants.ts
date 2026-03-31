63fa4cc2ce55f9279c7b8b98694298e56f892658
Subhadeep Roy <subha9.5roy350@gmail.com>
2026-03-31 13:59:26 +0530

    feat: add community tweet
    https://x.com/wiedymi/status/2038317908225634674

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,6 +17,7 @@ export const COMMUNITY_TWEET_URLS: readonly string[] = [
   "https://x.com/_chenglou/status/2037713766205608234",
   "https://x.com/birdabo/status/2038219452337074677",
   // ← BOT_INJECT_ANCHOR (do not remove)
+  "https://x.com/wiedymi/status/2038317908225634674",
   "https://x.com/Yuchenj_UW/status/2038303942606803178",
   "https://x.com/AhmedShahnab/status/2038224658512625671",
   "https://x.com/RazberryChai/status/2038309448679321727",
